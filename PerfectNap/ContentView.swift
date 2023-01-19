ac7f6edbd2fa208f22fea040a7db1c6550925b2e
rmcsweeney <rmcsweeney@utexas.edu>
2023-01-18 18:15:19 -0600

    Initial Commit

---
+++ PerfectNap/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  PerfectNap
+//
+//  Created by Ryan McSweeney on 1/18/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ PerfectNap/PerfectNapApp.swift
@@ -0,0 +1,17 @@
+//
+//  PerfectNapApp.swift
+//  PerfectNap
+//
+//  Created by Ryan McSweeney on 1/18/23.
+//
+
+import SwiftUI
+
+@main
+struct PerfectNapApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
