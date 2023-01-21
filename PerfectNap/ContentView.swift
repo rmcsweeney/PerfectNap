cbe9dc667b051300c788693100f4a8aa0f97ed9f
rmcsweeney <rmcsweeney@utexas.edu>
2023-01-21 14:24:10 -0600

    Push notifications sending

--- PerfectNap/ContentView.swift
+++ PerfectNap/ContentView.swift
@@ -5,18 +5,30 @@
 //  Created by Ryan McSweeney on 1/18/23.
 //  Sources used:
 //  For notifications https://stackoverflow.com/questions/68191982/swiftui-ask-notification-permission-on-start-without-button
+//  https://developer.apple.com/documentation/usernotifications/declaring_your_actionable_notification_types
 
 import SwiftUI
 import NotificationCenter
 
+let DEBUG = true
+
+let snoozeAction = UNNotificationAction(identifier: "SNOOZE", title: "Snooze", options: [])
+let turnOffAction = UNNotificationAction(identifier: "ALARM_OFF", title: "Turn Off", options: [])
+let alarmCategory = UNNotificationCategory(identifier: "ALARM_ACTION", actions: [snoozeAction, turnOffAction], intentIdentifiers: [], hiddenPreviewsBodyPlaceholder: "", options: .customDismissAction)
+
+let sleepContent = UNMutableNotificationContent()
+let wakeContent = UNMutableNotificationContent()
+
+
 struct ContentView: View {
+    
     let center = UNUserNotificationCenter.current()
-    let sleepContent = UNMutableNotificationContent()
-    let wakeContent = UNMutableNotificationContent()
+    
+
     
     
     //Set timer interval "every" and tolerance (tolerance at 1/10 good for preserving device power)
-    let timer  = Timer.publish(every: 300, tolerance: 30, on: .main, in: .common).autoconnect()
+    let timer  = Timer.publish(every: 3, tolerance: 3, on: .main, in: .common).autoconnect()
     let every = 5 //every as minutes
     
     let fallAsleepReload = 5
@@ -25,6 +37,8 @@ struct ContentView: View {
     @State private var minutes = 0
     @State private var fallAsleepTime = 0
     @State private var alarmTime = 0
+    
+    @State private var currentNotif: String? = nil
     
     //unimplemented
     @State private var authorized = true
@@ -38,7 +52,7 @@ struct ContentView: View {
     
     @State private var userStatus = userState.idle
     
-    init() {
+    func requestNotifications() {
         center.requestAuthorization(options: [.sound, .alert, .badge], completionHandler: { (granted, error) in
             if let error = error{
                 print("Error in authorization: " + error.localizedDescription)
@@ -46,11 +60,17 @@ struct ContentView: View {
             if (!granted){
                 print("Access not granted.")
             }
+            else{
+                print("Access allowed")
+            }
         })
+        
+        center.setNotificationCategories([alarmCategory])
         
         sleepContent.title = "Still awake?"
         sleepContent.body = "Tap here"
         sleepContent.sound = UNNotificationSound.default
+        sleepContent.categoryIdentifier = "ALARM_ACTION"
         
         wakeContent.title = "Wake up!"
         wakeContent.body = "Alarm running"
@@ -94,10 +114,19 @@ struct ContentView: View {
                 Text("Notifications not authorized!")
                     .opacity(authorized ? 0:1)
             }.padding()
+            
+            if (DEBUG){
+                HStack{
+                    Button(action: notifyCheckSleeping){
+                        Text("Test sleepcheck")
+                    }
+                }
+            }
         }
     }
     
     func beginSleep(){
+        requestNotifications()
         fallAsleepTime = fallAsleepReload
         userStatus = userState.fallingAsleep
     }
@@ -132,12 +161,12 @@ struct ContentView: View {
     
     
     func notifyCheckSleeping(){
-        let sleepRequest = UNNotificationRequest(identifier: UUID().uuidString, content: sleepContent, trigger: nil)
-        center.add(sleepRequest)
+        let sleepRequest = UNNotificationRequest(identifier: UUID().uuidString, content: sleepContent, trigger: UNTimeIntervalNotificationTrigger(timeInterval: 5, repeats: false))
+        UNUserNotificationCenter.current().add(sleepRequest)
     }
     
     func notifyWakeUp(){
-        let wakeUpRequest = UNNotificationRequest(identifier: UUID().uuidString, content: wakeContent, trigger: nil)
+        let wakeUpRequest = UNNotificationRequest(identifier: UUID().uuidString, content: wakeContent, trigger: UNTimeIntervalNotificationTrigger(timeInterval: TimeInterval(minutes * hours * 60), repeats: false))
         center.add(wakeUpRequest)
     }
     
